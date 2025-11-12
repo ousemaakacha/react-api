2702752c3ba0fe7672d3d4b34659ec289fc28ad7
ousemaakacha <akachaousema@gmail.com>
2025-11-12 12:17:25 +0100

    feat: useState and variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <></>;
+  const [actor, setActors] = useState([]);
+  const [actress, setActress] = useState([]);
+  const male_actend = "https://lanciweb.github.io/demo/api/actors/";
+  const female_actend = "https://lanciweb.github.io/demo/api/actresses/";
+
+  return (
+    <>
+      <div>
+        <h1>ATTORI E ATTRICI</h1>
+        <div>
+          <ul>{}</ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
