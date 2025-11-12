b9cc6d1957a25ee49e3cb7194dfbdcb1b76b56c2
ousemaakacha <akachaousema@gmail.com>
2025-11-12 13:48:40 +0100

    feat: card creation for each actor

--- src/App.jsx
+++ src/App.jsx
@@ -23,18 +23,41 @@ function App() {
   return (
     <>
       <div>
-        <h1>ATTORI E ATTRICI</h1>
+        <h1 className="text-center my-3">ATTORI E ATTRICI</h1>
         <div>
-          <button onClick={handleClick}> Carica Attori </button>
-          <ul>
-            {actors.map((actor) => (
-              <li key={actor.id}>
-                <li>
-                  {actor.name} {actor.birth_year}
-                </li>
-              </li>
-            ))}
-          </ul>
+          {/* <button onClick={handleClick}> Carica Attori </button> */}
+          <div className="content">
+            <div className="row g-2 justify-content-center">
+              {actors.map((actor) => (
+                <div
+                  key={actor.id}
+                  class="card col-12 col-md-3 col-lg-2 mx-1 align-items-center"
+                >
+                  <img
+                    src={actor.image}
+                    className="img-fluid"
+                    style={{
+                      width: "150px",
+                      height: "200px",
+                      objectFit: "cover",
+                    }}
+                  />
+
+                  <div className="col">
+                    <div class="card-body">
+                      <h5 class="card-title">{actor.name}</h5>
+                      <p>{actor.biography}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">{actor.birth_year}</li>
+                      <li class="list-group-item">{actor.nationality}</li>
+                      <li class="list-group-item">{actor.awards}</li>
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
